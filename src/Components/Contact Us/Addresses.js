336ec8e80ce1258139a63b0716db6fa87829a131
TalalHabib123 <talalhabib101@gmail.com>
2023-07-27 18:34:39 +0500

    minor changes

--- src/Components/Contact Us/Addresses.js
+++ src/Components/Contact Us/Addresses.js
@@ -5,10 +5,10 @@ const Address=()=>{
     const [selectedLocation, setSelectedLocation] = useState('Islamabad'); // State to store the selected location
 
     const locations = [
-        { id: 1, name: 'Islamabad', address: '123 Islamabad Street' },
-        { id: 2, name: 'Karachi', address: '456 Karachi Avenue' },
-        { id: 3, name: 'Dubai', address: '789 Dubai Road' },
-        { id: 4, name: 'Lahore', address: '101 Lahore Lane' },
+        { id: 1, name: 'Islamabad' },
+        { id: 2, name: 'Karachi' },
+        { id: 3, name: 'Dubai' },
+        { id: 4, name: 'Lahore' },
     ];
 
     const handleLocationClick = (locationName) => {
@@ -32,12 +32,71 @@ const Address=()=>{
                 ))}
             </div>
             <div className="theAddress">
-                {selectedLocation && (
-                    <>
-                        <h2>{selectedLocation}</h2>
-                        <p>{locations.find((loc) => loc.name === selectedLocation).address}</p>
-                    </>
-                )}
+                {(selectedLocation==='Islamabad' && (
+                        <>
+                            <h1>
+                                Islamabad Office
+                            </h1>
+                            <p>
+                                3rd Floor, Beverley Centre, Jinnah Avenue, F-6/1
+                                Islamabad 44000-Pakistan
+                                <br/>
+                                Phone : (+92-51) 111-672-253 (111-ORACLE)
+                                <br/>
+                                Tel: (051) 2814481-89
+                                <br/>
+                                Fax: (+92-51) 2814481
+                            </p>
+                        </>
+                    )) ||
+                    (selectedLocation==='Karachi' && (
+                        <>
+                            <h1>
+                                Karachi Office
+                            </h1>
+                            <p>
+                                Head Office Four Square Plaza, 4th Floor, Building # 21A, 
+                                H.H. Farooqi Road Off Tariq Road, PECHS, Block II
+                                Karachi – 75400, Pakistan
+                                <br/>
+                                Phone : (+92-21) 111–ORACLE (111-672-253)
+                                <br/>
+                                Fax: (92-21) 34536441
+                            </p>
+                        </>
+                    ))  ||
+                    (selectedLocation==='Lahore' && (
+                        <>
+                            <h1>
+                                Lahore Office
+                            </h1>
+                            <p>
+                                58-59 Main Boulevard, 1st Floor,Cavalry Ground,
+                                Lahore Cantt- Pakistan
+                                <br/>
+                                Phone : (+92-42) 111-672-253, (111-ORACLE)
+                                <br/>
+                                Fax: (+92-42) 36660598
+                            </p>
+                        </>
+                    )) ||
+                    (selectedLocation==='Dubai' && (
+                        <>
+                            <h1>
+                                Dubai Office
+                            </h1>
+                            <p>
+                                Ora-Tech Systems LLC Unit No. 1403, 14th Floor, Fortune Executive Tower, Cluster T, Jumeirah Lake
+                                Towers, Al Thanyan, Dubai – U.A.E.
+                                <br/>
+                                Phone : +9714 518-6666
+                                <br/>
+                                Fax: +9714 227-0151
+                            </p>
+                        </>
+                    ))
+                }
+
             </div>
             <div className="row2">
                 {secondRowLocations.map((location) => (
--- src/Components/Service/Collapsed_Comp.js
+++ src/Components/Service/Collapsed_Comp.js
@@ -4,7 +4,20 @@ import { CSSTransition } from 'react-transition-group';
 import Custom_Timeline from './CustomTimeline';
 
 const CollapsibleComponent = () => {
-  const [visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState(false);
+
+    const handleClick=(targetId)=>{
+      console.log(targetId);
+      const targetElement = document.querySelector(targetId);
+
+      if (targetElement) {
+        const elementRect = targetElement.getBoundingClientRect();
+        const offsetTop = elementRect.top;
+        const middleOfScreen = window.innerHeight / 5;
+        const scrollPosition = offsetTop - middleOfScreen + window.scrollY;
+        window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
+      }
+    };
 
   return (
     <div className="collapsed-service">
@@ -26,6 +39,21 @@ const CollapsibleComponent = () => {
               {visible && <Custom_Timeline />}
         </div>
       </CSSTransition>
+      {visible && 
+        <div className='collapse-link-toggle'>
+              <CButton
+                  href="#"
+                  onClick={(event) => {
+                  event.preventDefault();
+                  setVisible(!visible);
+                  handleClick(".collapsed-service");
+                  }}
+                  className="collapsible-link"
+                  style={{ border: 'none' }}
+              >
+                Show Less Details
+              </CButton>
+          </div>}
     </div>
   );
 };
