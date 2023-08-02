50540cb79f43f5b4efa2418a4769ee268858ca53
TalalHabib123 <talalhabib101@gmail.com>
2023-08-02 23:32:52 +0500

    Update firebase.js

--- src/Firebase_Setup/firebase.js
+++ src/Firebase_Setup/firebase.js
@@ -4,7 +4,13 @@ import {getFirestore} from "firebase/firestore";
 import { getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-  //Add Your Own Firebase Config Here
+  apiKey: "AIzaSyB2aTrskc6jkzZuneiUFKbUO07Wk1mvdLY",
+  authDomain: "ora-tech-website-ceeb3.firebaseapp.com",
+  projectId: "ora-tech-website-ceeb3",
+  storageBucket: "ora-tech-website-ceeb3.appspot.com",
+  messagingSenderId: "32691709983",
+  appId: "1:32691709983:web:de5963755f894cd84d0d2a",
+  measurementId: "G-CXZ1V8RZC5"
 };
 
 // Initialize Firebase
